1a18d86d0e4e203742e9d539f91c960f82ec5db1
Jordan Gonzalez <114965788+insky7@users.noreply.github.com>
2026-08-17 21:00:14 -0700

    Add: binary-search - submission-22

---
+++ Data Structures & Algorithms/binary-search/submission-22.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn search(nums: Vec<i32>, target: i32) -> i32 {
+        let mut pointer = (0 as i32, nums.len() as i32 - 1);
+        while pointer.1 >= pointer.0 {
+            let a: i32 = (pointer.0 + pointer.1) / 2;
+            if nums[a as usize] == target {
+                return a;
+            } else if nums[a as usize] > target {
+                pointer.1 = a - 1;
+            } else if nums[a as usize] < target {
+                pointer.0 = a + 1;
+            } else {
+                return -1;
+            } 
+        }
+        -1
+    }
+}
